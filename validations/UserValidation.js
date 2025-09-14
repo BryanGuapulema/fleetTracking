2b0ef364d9207f0ba808d59585e0acc022c15254
BryanGuapulema <bryan19012003@gmail.com>
2025-09-14 16:13:55 -0500

    user validation

---
+++ validations/UserValidation.js
@@ -0,0 +1,11 @@
+import z from 'zod'
+
+const userSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+  username: z.string().min(1)
+})
+
+export function validateUser (object) {
+  return userSchema.safeParse(object)
+}
