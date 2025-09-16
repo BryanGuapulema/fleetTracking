0b7007f2b7d90ad714e3e322785e9fb1e9cff4c3
BryanGuapulema <bryan19012003@gmail.com>
2025-09-15 22:55:20 -0500

    vehicle Schema created

---
+++ Schemas/vehicleSchema.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+export const VehicleSchema = new mongoose.Schema({
+  line_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Line',
+    required: true
+  },
+  vehicle_code: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  status: {
+    type: String,
+    enum: ['active', 'delayed', 'out_of_service'],
+    default: 'active'
+  },
+  last_known_location: {
+    type: [Number],
+    required: true
+  },
+  occupancy_percentage: {
+    type: Number,
+    default: '20'
+  }
+}, {
+  timestamps: true
+})
