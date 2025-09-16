fe76e3b69c720b2b3d0380b0deb4c01a1d26652b
BryanGuapulema <bryan19012003@gmail.com>
2025-09-15 22:56:52 -0500

    vehicle routes add to app

--- config/app.js
+++ config/app.js
@@ -5,6 +5,7 @@ import { homeRoutes } from '../routes/homeRoutes.js'
 import { userRouter } from '../routes/userRoutes.js'
 import { lineRouter } from '../routes/lineRoutes.js'
 import { stationRouter } from '../routes/stationsRoutes.js'
+import { vehicleRouter } from '../routes/vehicleRoutes.js'
 
 export const app = express()
 
@@ -14,5 +15,6 @@ app.use(express.json())
 app.use('/api/users', userRouter)
 app.use('/api/lines', lineRouter)
 app.use('/api/stations', stationRouter)
+app.use('/api/vehicles', vehicleRouter)
 
 app.use(homeRoutes)
