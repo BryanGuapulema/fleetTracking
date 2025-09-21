1f1d0e69012ab1546b2549762ef4b3211b3673c1
BryanGuapulema <bryan19012003@gmail.com>
2025-09-20 22:57:58 -0500

    line routes role definition

--- routes/lineRoutes.js
+++ routes/lineRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { LineController } from '../Controllers/LineController.js'
+import { authorizeRoles } from '../middlewares/roleMiddleware.js'
 
 export const lineRouter = Router()
 
-lineRouter.get('/', LineController.getAllLines)
-lineRouter.post('/', LineController.createLine)
-lineRouter.get('/:id', LineController.getLineById)
-lineRouter.put('/:id', LineController.updateLine)
-lineRouter.delete('/:id', LineController.deleteLine)
+lineRouter.get('/', authorizeRoles('admin'), LineController.getAllLines)
+lineRouter.post('/', authorizeRoles(['admin', 'operator']), LineController.createLine)
+lineRouter.get('/:id', authorizeRoles('admin'), LineController.getLineById)
+lineRouter.put('/:id', authorizeRoles(['admin', 'operator']), LineController.updateLine)
+lineRouter.delete('/:id', authorizeRoles('admin'), LineController.deleteLine)
