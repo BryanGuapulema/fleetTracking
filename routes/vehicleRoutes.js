4de7e900ebfd1f925e8b783d386e7bef1af94dd1
BryanGuapulema <bryan19012003@gmail.com>
2025-09-20 22:58:28 -0500

    vehicle routes role definition

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { VehicleController } from '../Controllers/VehicleController.js'
+import { authorizeRoles } from '../middlewares/roleMiddleware.js'
 
 export const vehicleRouter = Router()
 
-vehicleRouter.get('/', VehicleController.getAllVehicles)
-vehicleRouter.post('/', VehicleController.addVehicle)
-vehicleRouter.get('/:id', VehicleController.getVehicleById)
-vehicleRouter.put('/:id', VehicleController.updateVehicle)
-vehicleRouter.delete('/:id', VehicleController.deleteVehicle)
+vehicleRouter.get('/', authorizeRoles(['admin', 'operator']), VehicleController.getAllVehicles)
+vehicleRouter.post('/', authorizeRoles(['admin', 'operator']), VehicleController.addVehicle)
+vehicleRouter.get('/:id', authorizeRoles(['admin', 'operator']), VehicleController.getVehicleById)
+vehicleRouter.put('/:id', authorizeRoles(['admin', 'operator']), VehicleController.updateVehicle)
+vehicleRouter.delete('/:id', authorizeRoles(['admin', 'operator']), VehicleController.deleteVehicle)
